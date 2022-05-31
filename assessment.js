a4b73533d36ad49d841ed2797390a5f476bc596a
130ka <isao.kakinuma@gmail.com>
2022-05-31 22:08:48 +0900

    aaa

--- assessment.js
+++ assessment.js
@@ -11,14 +11,27 @@ assessmentButton.onclick = () => {
     }
 
     resultDivided.innerText = '';
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivided.appendChild(header);
-
+    const headerDivided = document.createElement('div');
+    headerDivided.setAttribute('class', 'card-header');
+    headerDivided.innerText = '診断結果';
+  
+    // bodyDivided の作成
+    const bodyDivided = document.createElement('div');
+    bodyDivided.setAttribute('class', 'card-body');
+  
     const paragraph = document.createElement('p');
-    const result = assessmet(userName);
+    paragraph.setAttribute('class', 'card-text');
+    const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivided.appendChild(paragraph);
+    bodyDivided.appendChild(paragraph);
+  
+    // resultDivided に Bootstrap のスタイルを適用する
+    resultDivided.setAttribute('class', 'card');
+    resultDivided.setAttribute('style', 'max-width: 700px;')
+  
+    // headerDivided と bodyDivided を resultDivided に差し込む
+    resultDivided.appendChild(headerDivided);
+    resultDivided.appendChild(bodyDivided);
 
     tweetDivided.innerText = '';
     const anchor = document.createElement('a');
@@ -63,7 +76,7 @@ const answers = [
  * @return {string} 診断結果
  */
 
-function assessmet(userName){
+function assessment(userName){
     let sumOfCharCode = 0;
     for(let i = 0; i < userName.length; i++){
         sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
